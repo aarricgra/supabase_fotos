ebda80475d13341fada37c9b577c22830ee5e741
aarricgra <aarricgra@alu.edu.gva.es>
2024-01-24 10:18:49 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>https://msydhfyerbwrlyegtfav.supabase.co/rest/v1/Fotos?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zeWRoZnllcmJ3cmx5ZWd0ZmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5NjkzODQsImV4cCI6MjAyMDU0NTM4NH0.qYAZVb5bvIumtrDhvYZf_A0MtZnyCLOtQWTaWy3pb1Y</h1>
+  </main>
+</template>
+
+<script>
+
+</script>
